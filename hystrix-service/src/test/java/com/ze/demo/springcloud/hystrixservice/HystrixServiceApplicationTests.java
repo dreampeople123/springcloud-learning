eefcca6e3e978ec5d5e635fe2956ebf5c0ab066d
gaozien <Lh159263>
2021-03-31 16:40:23 +0800

    添加hystrixService

---
+++ hystrix-service/src/test/java/com/ze/demo/springcloud/hystrixservice/HystrixServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ze.demo.springcloud.hystrixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
